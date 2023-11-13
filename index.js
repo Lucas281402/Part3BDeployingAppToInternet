7c9af2496446f32b36b091622fa9915b97ee68cd
Lucas <lucas_river_2814@hotmail.com>
2023-11-13 07:52:42 -0300

    commit of validations

--- index.js
+++ index.js
@@ -41,42 +41,44 @@ app.get("/api/notes", (request, response) => {
 });
 
 app.delete("/api/notes/:id", (request, response, next) => {
-  Note.findByIdAndDelete(request.params.id).then(result => {
-    response.status(204).end()
-  }).catch(error => next(error))
+  Note.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
-app.post("/api/notes", (request, response) => {
+app.post("/api/notes", (request, response, next) => {
   const body = request.body;
-
-  if (body.content === undefined) {
-    return response.status(400).json({
-      error: "content missing",
-    });
-  }
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
   });
 
-  note.save().then((savedNote) => {
-    response.json(savedNote);
-  });
+  note
+    .save()
+    .then((savedNote) => {
+      response.json(savedNote);
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
 
-app.put('/api/notes/:id', (request, response) => {
-  const body = request.body
+app.put("/api/notes/:id", (request, response, next) => {
+  const { content, important } = request.body;
 
-  const note = {
-    content: body.content,
-    important: body.important,
-  }
-  
-  Note.findByIdAndUpdate(request.params.id, note, { new: true }).then(updatedNote => {
-    response.json(updatedNote)
-  }).catch(error => next(error))
-})
+  Note.findByIdAndUpdate(
+    request.params.id,
+    { content, important },
+    { new: true, runValidators: true, context: "query" }
+  )
+    .then((updatedNote) => {
+      response.json(updatedNote);
+    })
+    .catch((error) => next(error));
+});
 
 app.use(unknownEndpoint);
 
@@ -84,6 +86,8 @@ const errorHandler = (error, request, response, next) => {
   console.error(error.message);
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
   }
   next(error);
 };
